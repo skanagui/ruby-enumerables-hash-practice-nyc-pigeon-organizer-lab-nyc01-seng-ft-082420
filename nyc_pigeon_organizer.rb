1b1469afafd971256ffb8a4870f96d8f6260fe5f
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-18 19:11:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,24 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  
-  data.each_with_object({}) do |(key, value), final_array|
-    #binding.pry 
-    value.each do () |inner_key, names| 
-    binding.pry 
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
   end
- final_array
-end   
-  binding.pry 
-end 
+  new_hash
+end
 
   
 
